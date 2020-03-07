c78eb47edf393f8c83842d2961f0b5facb35c90c
Emiliano Corvalán <corvalanemiliano@hotmail.com>
2020-03-07 00:42:44 -0300

    primer commit proyecto laravel 5.8

---
+++ refiriendo/resources/views/categorias.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Categorias</title>
+  </head>
+  @include('./header')
+  <body>
+    <h1>categorias</h1>
+    <p> "Acá deberíamos recorrer y mostar todos las categorias de la bbdd"</p>
+  </body>
+  @include('./footer')
+</html>
